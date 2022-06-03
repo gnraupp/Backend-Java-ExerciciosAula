a4405d50c9d93ef5cc2c1b5aad1445a29e879940
gnraupp <106340041+gnraupp@users.noreply.github.com>
2022-06-03 17:52:24 -0300

    Aula 3

---
+++ Aula3/Aula3.java
@@ -0,0 +1,23 @@
+package com.stefanini.Aula2;
+
+public class Aula3 {
+public static void main(String[] args) {
+	byte numByte = - 128; // - 128 a 127 (8 bits)
+	short numShort = 12321; // -32768 a 32767 (16 bits)
+	int numInt = 2147483647; // -2.147.483.648 a 2.147.483.647 (32 bits)
+	long numLong = 2147483647; // -9.223.372.036.854.770.000 a 9.223.372.036.854.770.000 (64 bits)
+	
+	float numFloat = 10.000f; // -1,402E-37 a 3.40282347E+38 (32 bits)
+	double numDouble = 100000000000000.000; // -4,94E-307 a 1.79769313486231570E+308 (64 bits)
+	
+	char varChar = 'F'; // 0 a 65535 (16 bits)
+	
+	boolean varBoolean = true; // true or false (1 bit)
+	
+	String texto = "Bem vindos Devs!"; 
+	Integer numeroConta = 4000;
+	Long numeroLongo = 1213123L;
+	
+	String nomeCompleto = "Giovana";
+}
+}
