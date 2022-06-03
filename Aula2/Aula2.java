bacd910a0fa095d39806f6cf2edc9074ec8ef6d5
gnraupp <106340041+gnraupp@users.noreply.github.com>
2022-06-03 17:51:49 -0300

    Aula 2

---
+++ Aula2/Aula2.java
@@ -0,0 +1,15 @@
+package com.stefanini.Aula2;
+
+public class Aula2 {
+
+	public static void main(String[] args) {
+	
+		int nota = 7;
+		String nome = "10";
+		
+		System.out.println(nota);
+		System.out.println(nome);
+		
+	}	
+
+}
---
+++ Aula2/Aula2class.java
@@ -0,0 +1,12 @@
+package com.stefanini.Aula2;
+
+public class Aula2class {
+	
+	public static void main(String[] args) {
+		Aula2 aula = new Aula2();
+		System.out.println(aula);
+		
+		
+	}
+
+}
