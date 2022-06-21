d0189a09d659e58e7d614abe803e00a7de6ee798
gnraupp <106340041+gnraupp@users.noreply.github.com>
2022-06-21 15:34:01 -0300

    Aula 9

---
+++ Aula9/Carro.java
@@ -0,0 +1,19 @@
+package com.stefanini.Aula9;
+
+public class Carro {
+	
+	String marca;
+	String modelo;
+	int velocidadeMaxima;
+	int taxaAcelaracao;
+	double quilometrosPorLitro;
+	
+	void acelerar() {
+		System.out.println("Acelerando...");
+	}
+	
+	void frear() {
+		System.out.println("Freando...");
+	}
+
+}
